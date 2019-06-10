f530d7c90ee16f5363e31e0caaeb0bffdcff18d0
Magesh B <magesh.madz@gmail.com>
2019-06-10 23:09:37 +0530

    Adding shooting sound

--- script.js
+++ script.js
@@ -11,3 +11,70 @@
   })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
   ga('create', 'UA-16233284-1', 'auto');
   ga('send', 'pageview');
+
+  /*$(window).load(()=>{
+    /*var bgm = document.getElementById('bg-music');
+    bgm.play().catch(function(){
+      console.log("Shit Happens!");
+    });
+  });*/
+
+
+  
+  var shootingSound = new Audio('shoot.wav');
+  $(window).on('load',function(){
+    var bgCanvas = document.getElementById(".column");
+    /*
+    bgCanvas.addEventListener('click', shot);*/
+    $(bgCanvas).on('click',shot);
+
+  });
+/*
+  function inity()
+  {
+    /* War plane shake motion
+    var randomShake = Math.floor(Math.random()*6)+1;
+    console.log(randomShake);
+    //document.getElementById('#enemy-'+randomShake).classList.toggle('shake');
+    //$('namme').toggleclass('shake');
+    document.querySelector('#enemy-'+randomShake).classList.toggle('shake');
+
+  }
+
+  document.querySelector('#enemy-1').addEventListener('click',function(){
+    inity();
+    shootingSound.play();
+  });
+
+
+
+/*
+  var shootingSound = new Audio('shoot.wav');
+  var bgCanvas = document.getElementById("#warBg");
+  bgCanvas.addEventListener("click", function (){
+    shootingSound.play();
+  });*/
+
+function shot()
+{
+  shooty.play();
+  //shootingSound.play();
+}
+
+  /*
+  $(warBg).on('click', ()=>{
+    shootingSound.play();
+  });
+*/
+
+/*
+var promise = document.querySelector('bg-music').play();
+if (promise !== undefined) {
+    promise.then(_ => {
+        // Autoplay started!
+      
+    }).catch(error => {
+        // Autoplay was prevented.
+        // Show a "Play" button so that user can start playback.
+    });
+}*/
